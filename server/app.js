8254abdbc339f0a70128aef5b3b0451d402e9429
kristenoom <kris.a.oom@gmail.com>
2021-08-10 17:06:06 -0400

    testing server, uploading screenshots of successful tests

---
+++ server/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+let express = require("express");
+let app = express();
+let sequelize = require("./db");
+
+let user = require("./controllers/user-controller");
+let log = require('./controllers/log-controller');
+
+sequelize.sync();
+//sequalize.sync({force:true});
+
+app.use(express.json());
+//app.use(require("./middleware/headers"));
+
+//Exposed
+app.use("/user", user);
+
+//Protected
+//app.use(require('./middleware/validate-session'));
+app.use('/log', log);
+
+app.listen(3001, function () {
+  console.log("App is listening on port 3001");
+});
